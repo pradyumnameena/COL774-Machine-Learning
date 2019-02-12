039d499dd0dd32aa3678f3e041f359890c7c4995
Pradyumna Meena <pradyumnameena@gmail.com>
2019-02-12 19:23:47 +0530

    coding part complete. plotting is left

--- Assignment-1/gda.py
+++ Assignment-1/gda.py
@@ -46,34 +46,36 @@ def compute_mean(x,y,num_classes):
       summation[i,j]/=count[i,0]
   return summation
 
-def compute_var(x,y,num_classes):
+def compute_var(x,y,num_classes,mean_matrix):
+  rv = np.asmatrix(np.zeros((mean_matrix.shape[0],mean_matrix.shape[1]),dtype=float,order='F'))
   count = np.asmatrix(np.zeros((num_classes,1),dtype=int,order='F'))
-  summation = np.asmatrix(np.zeros((num_classes,x.shape[1]),dtype=float,order='F'))
   for i in range(len(x)):
-    count[y[i],0]+=1
+    count[y[i]]+=1
     for j in range(x.shape[1]):
-      summation[y[i],j]+=x[i,j]
+      rv[y[i],j]+=(x[i,j]-mean_matrix[y[i],j])*(x[i,j]-mean_matrix[y[i],j])
+  
   for i in range(num_classes):
     for j in range(x.shape[1]):
-      summation[i,j]/=count[i,0]
-  return summation
+      rv[i,j]/=count[y[i],0]
+  return rv
 
-def compute_covariance_matrix(x,y,mean_mat,var_mat):
+def compute_covariance_matrix(x,y,mean_mat,var_mat,class_id):
   z = np.asmatrix(np.zeros((x.shape[1],x.shape[1]),dtype=float,order='F'))
   for i in range(x.shape[1]):
-    z[i,i] = mean_mat[0,i]*mean_mat[0,i] + var_mat[0,i]
+    z[i,i] = mean_mat[class_id,i]*mean_mat[class_id,i] + var_mat[class_id,i]
   return z
 
 def main():
   (x,y) = read_params()
   num_classes = 2
   mean_matrix = compute_mean(x,y,num_classes)
-  variance_matrix = compute_var(x,y,num_classes)
+  variance_matrix = compute_var(x,y,num_classes,mean_matrix)
   mean_0 = mean_matrix[0,:]
   mean_1 = mean_matrix[1,:]
   var_0 = variance_matrix[0,:]
   var_1 = variance_matrix[1,:]
-  covariance_matrix = compute_covariance_matrix(x,y,mean_0,var_0)
+  covariance_matrix_0 = compute_covariance_matrix(x,y,mean_0,var_0,0)
+  covariance_matrix_1 = compute_covariance_matrix(x,y,mean_1,var_1,1)
 
 if __name__ == "__main__":
 	main()
