0751b027ea9a823b41d218f681742abc0e277634
Pradyumna Meena <pradyumnameena@gmail.com>
2019-02-13 16:08:01 +0530

    completed logistic regression. bugfixes and plots all done

--- Assignment-1/logistic_reg.py
+++ Assignment-1/logistic_reg.py
@@ -15,46 +15,28 @@ def read_params():
 def normalize(mat):
   mean = np.mean(mat,axis=0)
   var = np.var(mat,axis=0)
+  mat-=mean
   for i in range (len(mat)):
-  	for j in range (mat.shape[1]):
-  		mat[i,j] = Decimal(mat[i,j]) - Decimal(mean[0,j])
-  		mat[i,j] = Decimal(mat[i,j])/Decimal(var[0,j])
-  z = np.zeros((mat.shape[0],1),dtype=float)
-  z+=1
+    mat[i,:] = np.divide(mat[i,:],var)
+  z = 1 + np.zeros((mat.shape[0],1),dtype=float)
   z = np.hstack((z,mat))
   return z
 
 def algo(x,y,theta):
-  prod = 1/(1+np.exp(-1*np.dot(x,theta)))
-  prod1 = np.multiply(y,np.log(prod))
-  prod2 = np.multiply(1-y,np.log(1 - prod))
-  pred = prod1+prod2
-  prod3 = np.multiply(prod,1-prod)
-  prod = y - prod
-  
-  first_der = np.asmatrix(np.zeros((x.shape[0],x.shape[1]),dtype=float,order='F'))
-  for i in range(len(x)):
-    for j in range(x.shape[1]):
-      first_der[i,j] = x[i,j] * prod[i,0]
-  first_der = np.sum(first_der,axis=0).transpose()
+  prediction = 1/(1+np.exp(-1*np.dot(x,theta)))
+  h_1_h = np.multiply(prediction,1-prediction)
+  diff = y - prediction
+  first_der = np.dot(x.transpose(),diff)
   
   second_der = np.asmatrix(np.zeros((x.shape[1],x.shape[1]),dtype=float,order='F'))
   for i in range(x.shape[1]):
     for j in range(x.shape[1]):
       for k in range(len(x)):
-        second_der[i,j]+=x[k,i]*x[k,j]*prod3[k,0]
+        second_der[i,j]+=x[k,i]*x[k,j]*h_1_h[k,0]
   second_der = np.linalg.pinv(-1*second_der)
-  
   theta-=np.dot(second_der,first_der)
-  return
 
-def main():
-  (x,y,x_data,y_data) = read_params()
-  theta = np.asmatrix(np.zeros((x.shape[1],1),dtype=float,order='F'))
-  algo(x,y,theta)
-  print(theta)
-  val = np.dot(x,theta)
-  val = 1/(1+np.exp(-1*val))
+def curve_plot(x,y,theta):
   class_1 = []
   class_2 = []
   for i in range(len(x)):
@@ -64,13 +46,22 @@ def main():
       class_2.append([x[i,1],x[i,2]])
   plt.scatter(np.array(class_1)[:,0],np.array(class_1)[:,1],c='g',label='positive class (y==1)')
   plt.scatter(np.array(class_2)[:,0],np.array(class_2)[:,1],c='r',label='negative class (y==0)')
-  x_1 = np.arange(-2,3,0.1)
+  x_1 = np.arange(-3,3,0.01)
   y_1 = -(theta[0,0] + theta[1,0]*x_1)/theta[2,0]
   plt.plot(x_1,y_1)
+  plt.xlabel('x1')
+  plt.ylabel('x2')
   plt.legend()
-  plt.show()
-  val = np.hstack((val,y))
-  print(val)
+  plt.title('Logistic Regression')
+  plt.savefig('logistic_reg.png',dpi=200)
+
+def main():
+  (x,y,x_data,y_data) = read_params()
+  theta = np.asmatrix(np.zeros((x.shape[1],1),dtype=float,order='F'))
+  algo(x,y,theta)
+  # val = 1/(1+np.exp(-1*np.dot(x,theta)))
+  # print(val)
+  curve_plot(x,y,theta)
 
 if __name__ == "__main__":
 	main()
