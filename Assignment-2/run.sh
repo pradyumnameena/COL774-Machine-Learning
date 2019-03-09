c781ff70d3da0afc690ac174d366518c17074227
Pradyumna Meena <pradyumnameena@gmail.com>
2019-03-09 23:31:28 +0530

    added run.sh and corrected binary classification code

---
+++ Assignment-2/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$1" == "1" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" != "" ]; then
+	python naive_bayes.py $2 $3 $4
+elif [ "$1" == "2" ] && [ "$2"!="" ] && [ "$3" != "" ] && [ "$4" != "" ] && [ "$5" != "" ]; then
+	python svm_col774.py $2 $3 $4 $5
+else
+	echo "Supply appropriate and correct arguments"
+	exit
+fi
--- Assignment-2/svm.py
+++ Assignment-2/svm.py
@@ -1,90 +1,85 @@
+import sys
 import csv
 import pandas as pd
 import numpy as np
 import cvxopt
-import cvxopt.solvers
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 from svmutil import *
 
 # getting the training data
-def get_train_params(issubset):
-	train_data = pd.read_csv('../../ass2_data_svm/train.csv',header=None,dtype=float)
+def get_train_params(train_data_path,issubset):
+	train_data = np.array(pd.read_csv(train_data_path,header=None,dtype=float).values)
+	train_output = np.array(train_data[:,784:785])
+	
 	if issubset==True:
-		train_data = train_data.loc[(train_data[784] == 5) | (train_data[784] == 6)].values
-	train_output = train_data[:,784:785]
-	for i in range(len(train_output)):
-		if train_output[i,0] == 5:
-			train_output[i,0] = 1
-		else:
-			train_output[i,0] = -1
-	train_data = train_data[:,0:784]
+		print("treating 5 as class 1 and 6 as class -1")
+		train_data = train_data[np.ix_((train_data[:,784]==5) | (train_data[:,784]==6))]
+		train_output = 11 - 2*train_data[:,784:785]
+
 	train_data = train_data/256
-	return (train_data,train_output)
+	return (np.asmatrix(train_data[:,0:784]),np.asmatrix(train_output))
 
 # get the testing data
-def get_test_params(issubset):
-	test_data = pd.read_csv('../../ass2_data_svm/test.csv',header=None,dtype=float)
+def get_test_params(test_data_path,issubset):
+	test_data = np.array(pd.read_csv(test_data_path,header=None,dtype=float).values)
+	test_output = np.array(test_data[:,784:785])
+
 	if issubset==True:
-		test_data = test_data.loc[(test_data[784] == 5) | (test_data[784] == 6)].values
-	test_output = test_data[:,784:785]
-	for i in range(len(test_output)):
-		if test_output[i,0] == 5:
-			test_output[i,0] = 1
-		else:
-			test_output[i,0] = -1
-	test_data = test_data[:,0:784]
-	test_data = test_data[:,0:784]/256
-	return (test_data,test_output)
+		test_data = test_data[np.ix_((test_data[:,784]==5) | (test_data[:,784]==6))]
+		test_output = 11 - 2*test_data[:,784:785]
+	
+	test_data = test_data/256
+	return (np.asmatrix(test_data[:,0:784]),np.asmatrix(test_output))
 
 # linear kernel
-def linear_kernel_cvxopt(train_data,train_output):
-	# for cvxopt use
+def linear_kernel_cvxopt(train_data,train_output,penalty):
 	m = len(train_data)
 	X_Y = np.multiply(train_data,train_output)
 	
 	P = cvxopt.matrix(np.dot(X_Y,X_Y.transpose()))
-	q = cvxopt.matrix(-2*np.ones((m,1)))
+	q = cvxopt.matrix(-1*np.ones((m,1)))
 	A = cvxopt.matrix(train_output.transpose())
 	b = cvxopt.matrix(0.0)
 
 	tmp1 = -1*np.identity(m)
 	tmp2 = np.identity(m)
 	G = cvxopt.matrix(np.vstack((tmp1,tmp2)))
-	tmp1 = np.zeros(m)
-	tmp2 = np.ones(m)
-	h = cvxopt.matrix(np.hstack((tmp1,tmp2)))
+	tmp1 = np.zeros((m,1))
+	tmp2 = penalty*np.ones((m,1))
+	h = cvxopt.matrix(np.vstack((tmp1,tmp2)))
 	solution = cvxopt.solvers.qp(P,q,G,h,A,b)
 	return solution
 
 # gaussian function calculator
 def gaussain_func(a,b,gamma):
 	diff = a-b
-	rv = np.exp(-1*gamma*float(np.sum(diff*diff)))
+	rv = np.exp(-1*gamma*float(np.sum(np.multiply(diff,diff))))
 	return rv
 
 # gaussian kernel
 def gaussian_kernel_cvxopt(train_data,train_output,gamma):
-	# for cvxopt use
 	m = len(train_data)
-	n = train_data.shape[1]
-	X_Y = np.asmatrix(np.zeros((m,n),dtype=float))
+	kernel = np.asmatrix(np.zeros((m,m),dtype=float))
 	for i in range(m):
-		for j in range(n):
-			X_Y[i,j] = gaussain_func(train_data[i,:],train_data[j,:],gamma)*train_output[i]
-			# X_Y[i,j] = gaussain_func(train_data[i,:],train_data[j,:],gamma)*train_output[i]*train_output[j]
-	
-	P = cvxopt.matrix(np.dot(X_Y,X_Y.transpose()))
-	q = cvxopt.matrix(-2*np.ones((m,1)))
+		for j in range(m):
+			kernel[i,j] = gaussain_func(train_data[i,:],train_data[j,:],gamma)
+			# print(str(i) + " " + str(j))
+			# diff = train_data[i,:] - train_data[j,:]
+			# kernel[i,j] = np.sum(np.dot(diff,diff.transpose))
+	# kernel = np.exp(-1*gamma*kernel)
+
+	P = cvxopt.matrix(np.multiply(kernel,np.dot(train_output,train_output.transpose())))
+	q = cvxopt.matrix(-1*np.ones((m,1)))
 	A = cvxopt.matrix(train_output.transpose())
 	b = cvxopt.matrix(0.0)
 
 	tmp1 = -1*np.identity(m)
 	tmp2 = np.identity(m)
 	G = cvxopt.matrix(np.vstack((tmp1,tmp2)))
-	tmp1 = np.zeros(m)
-	tmp2 = np.ones(m)
-	h = cvxopt.matrix(np.hstack((tmp1,tmp2)))
+	tmp1 = np.zeros((m,1))
+	tmp2 = np.ones((m,1))
+	h = cvxopt.matrix(np.vstack((tmp1,tmp2)))
 	solution = cvxopt.solvers.qp(P,q,G,h,A,b)
 	return solution
 
@@ -94,92 +89,93 @@ def calculate_svm_params(kernel_soln,train_data,train_output,tolerance):
 	raveled = np.ravel(kernel_soln['x'])
 	langrangian_params = np.arange(len(raveled)) [raveled>tolerance]
 	weight_matrix = np.asmatrix(np.zeros((1,n),dtype=float))
-	
+	nSV = 0
 	for i in langrangian_params:
+		nSV+=1
 		for j in range(n):
 			weight_matrix[0,j]+=(raveled[i]*train_data[i,j]*train_output[i,0])
 	
 	idx_used_for_b = langrangian_params[0]
 	b = train_output[idx_used_for_b] - np.dot(train_data[idx_used_for_b,:],weight_matrix.transpose())[0,0]
-	
-	return (weight_matrix,b)
+	return (weight_matrix,b,nSV)
 
 # predicting using parameters supplied on the supplied test_data
 def svm_prediction(weight_matrix,b,test_data):
 	predicted = np.asmatrix(np.zeros((len(test_data),1),dtype=int))
-	for i in range(len(test_data)):
-		val = np.dot(test_data[i,:],weight_matrix.transpose())[0,0] + b
-		if val>0:
-			predicted[i] = 1
-		else:
-			predicted[i] = -1
+	val = np.dot(test_data,weight_matrix.transpose()) + b
+	predicted = 2*np.multiply((val>0),np.ones((len(test_data),1))) - 1
 	return predicted
 
 # libsvm package
-def linear_kernel_libsvm(train_data,train_output,test_data,test_output):
-	
+def linear_kernel_libsvm(train_data,train_output,test_data,test_output,gamma,penalty):
 	train_labels = []
 	train_input = train_data.tolist()
-	for j in train_output:
-		train_labels.extend(j)
-
+	for j in range(train_output.shape[0]):
+		train_labels.append(train_output[j,0])
+	
 	test_labels = []
 	test_input = test_data.tolist()
-	for j in test_output:
-		test_labels.extend(j)
-	
+	for j in range(test_output.shape[0]):
+		test_labels.append(test_output[j,0])
 	problem = svm_problem(train_labels,train_input)
-	param = svm_parameter("-s 0 -c 1 -t 0")
-	model = svm_train(problem,param)
-	pred_lbl, pred_acc, pred_val = svm_predict(test_labels,test_input,model)
+	
+	linear_param = svm_parameter("-s 0 -c 1 -t 0")
+	linear_model = svm_train(problem,linear_param)
+	linear_pred_lbl, linear_pred_acc, linear_pred_val = svm_predict(test_labels,test_input,linear_model)
+
+	gaussian_param = svm_parameter("-s 0 -c " + str(penalty) + " -t 2 -g " + str(gamma))
+	gaussian_model = svm_train(problem,gaussian_param)
+	gaussian_pred_lbl, gaussian_pred_acc, gaussian_pred_val = svm_predict(test_labels,test_input,gaussian_model)
 
 # main function
 def main():
-	print("treating 5 as class 1 and 6 as class -1")
+
+	# taking parameters from command line
+	train_data_path = sys.argv[1]
+	test_data_path = sys.argv[2]
+	classification = sys.argv[3]
+	part = sys.argv[4]
+
+	# reading train and test data as array
+	issubset = (classification=='0')
+	print("reading data with normalization")
+	(train_data,train_output) = get_train_params(train_data_path,issubset)
+	(test_data,test_output) = get_test_params(test_data_path,issubset)
 	
-	(train_data,train_output) = get_train_params(True)
-	print("normalized training data retrieved")
-
-	(test_data,test_output) = get_test_params(True)
-	print("normalized test data retrieved")
-
-	# linear_kernel_soln = linear_kernel_cvxopt(train_data,train_output)
-	print("linear kernel solution found")
+	if part == 'a':
+		tolerance = 1e-4
+		penalty = 1
+		print("tolerance value for linear kernel = " + str(tolerance))
+		linear_kernel_soln = linear_kernel_cvxopt(train_data,train_output,penalty)
+		(weight_matrix,b,nSV) = calculate_svm_params(linear_kernel_soln,train_data,train_output,tolerance)
+		print(str(nSV) + " support vectors")
+		predicted = svm_prediction(weight_matrix,b,test_data)
+		confatrix = confusion_matrix(test_output,predicted)
+		print(confatrix)
 	
-	tolerance = 1e-8
-	# (weight_matrix,b) = calculate_svm_params(linear_kernel_soln,train_data,train_output,tolerance)
-	print("svm parameters computed")
-
-	# predicted = svm_prediction(weight_matrix,b,test_data)
-	print("prediction complete")
-
-	# confatrix = confusion_matrix(test_output,predicted)
-	print("confusion matrix computed")
+	elif part =='b':
+		gamma = 0.05
+		tolerance = 1e-4
+		print("tolerance value for gaussian kernel = " + str(tolerance))
+		gaussian_kernel_soln = gaussian_kernel_cvxopt(train_data,train_output,gamma)
+		(weight_matrix,b,nSV) = calculate_svm_params(gaussian_kernel_soln,train_data,train_output,tolerance)
+		print(str(nSV) + " support vectors")
+		predicted = svm_prediction(weight_matrix,b,test_data)
+		confatrix = confusion_matrix(test_output,predicted)
+		print(confatrix)
 	
-	print("below is confusion matrix for linear kernel")
-	# print(confatrix)
-
-	# below is for gaussian kernel
-	gamma = 0.05
-	# gaussian_kernel_soln = gaussian_kernel_cvxopt(train_data,train_output,gamma)
-	print("gaussian kernel solution found")
+	elif part == 'c':
+		gamma = 0.05
+		penalty = 1
+		linear_kernel_libsvm(train_data,train_output,test_data,test_output,gamma,penalty)
+		print("c")
 	
-	tolerance = 1e-8
-	# (weight_matrix,b) = calculate_svm_params(gaussian_kernel_soln,train_data,train_output,tolerance)
-	print("svm parameters computed")
-
-	# predicted = svm_prediction(weight_matrix,b,test_data)
-	print("prediction complete")
-
-	# confatrix = confusion_matrix(test_output,predicted)
-	print("confusion matrix computed")
+	elif part=='d':
+		print("d")
 	
-	print("below is confusion matrix for gaussian kernel")
-	# print(confatrix)
-
-
-	# below id for libsvms
-	linear_kernel_libsvm(train_data,train_output,test_data,test_output)
+	else:
+		print("Part number is incorrect")
+	
 	return
 
 if __name__ == "__main__":
