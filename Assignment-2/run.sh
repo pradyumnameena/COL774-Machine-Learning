af638ad2bf1f8248ac686cacdf561cdd06cb167b
Pradyumna Meena <pradyumnameena@gmail.com>
2019-03-29 15:41:12 +0530

    modified run.sh

--- Assignment-2/run.sh
+++ Assignment-2/run.sh
@@ -2,7 +2,7 @@
 if [ "$1" == "1" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" != "" ]; then
 	python naive_bayes.py $2 $3 $4
 elif [ "$1" == "2" ] && [ "$2"!="" ] && [ "$3" != "" ] && [ "$4" != "" ] && [ "$5" != "" ]; then
-	python svm_col774.py $2 $3 $4 $5
+	python svm.py $2 $3 $4 $5
 else
 	echo "Supply appropriate and correct arguments"
 	exit
