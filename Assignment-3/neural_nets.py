f77f43899be4c2004cc23f0a6f056fac8fe48566
Pradyumna Meena <pradyumnameena@gmail.com>
2019-04-02 23:57:10 +0530

    neural nets file updated

--- Assignment-3/neural_nets.py
+++ Assignment-3/neural_nets.py
@@ -15,8 +15,20 @@ def read_file(datapath):
 def sigmoid_activation(a):
 	return 1/(1+np.exp(-1*a))
 	
+def sigmoid_derivative(a):
+	return np.multiply(sigmoid(a),1-sigmoid(a))
+
 def relu_activation(a):
-	return max(0,a)
+	return np.maximum(0,a)
+
+def relu_derivative(a):
+	return 0
+
+def modify_output(data_y,num_classes):
+	output_mat = np.asmatrix(np.zeros((len(data_y),num_classes),dtype=int))
+	for i in range(len(data_y)):
+		output_mat[i,data_y[i,0]]=1
+	return output_mat
 	
 def initialize_params(arch_details):
 	params = {}
@@ -26,14 +38,28 @@ def initialize_params(arch_details):
 	return params
 	
 def forward_prop(params,data_x,activation):
-	return
+	forward_pass = {}
+	num_layers = len(params)/2
+	x = np.transpose(data_x)
+	for i in range(num_layers):
+		x = np.dot(params["W"+str(i+1)],x) + params["b"+str(i+1)]
+		forward_pass["z"+str(i+1)] = x
+		x = sigmoid_activation(x)
+		forward_pass["a"+str(i+1)] = x
+	return forward_pass
+
+def cost_function(output_layer_output,actual_output):
+	loss = np.add(np.multiply(actual_output,np.log(output_layer_output)),np.multiply(1-actual_output,np.log(1-output_layer_output)))
+	return (-1*np.sum(loss,axis=1)/(output_layer_output.shape[1]))
 	
-def backward_prop():
-	return
-	
-def cost_function(output_layer_output,):
-	return
-	
+def backward_prop_sigmoid(params,cost,forward_pass,learning_rate):
+	der_output = forward_pass["a"+str(len(params)/2)] - 
+	return params
+
+def backward_prop_relu(params,cost,forward_pass,learning_rate):
+	der_output = forward_pass["a"+str(len(params)/2)] - 
+	return params
+
 def main():
 	train_datapath = "dataset/neural_net/poker-hand-training-true.data"
 	test_datapath = "dataset/neural_net/poker-hand-testing.data"
@@ -41,14 +67,20 @@ def main():
 	(test_x,test_y) = read_file(test_datapath)
 	
 	learning_rate = 0.01
-	architecture_details = [5,4]
+	architecture_details = [5]
 	num_outputs = 10
-	
+	num_iters = 100
+	modified_y = modify_output(train_y,num_outputs)
+
 	neurons_list = [train_x.shape[1]]
 	neurons_list.extend(architecture_details)
 	neurons_list.append(num_outputs)
 	
 	params = initialize_params(neurons_list)
+	for counter in range(num_iters):
+		forward_pass  = forward_prop(params,train_x,activation)
+		cost = cost_function(forward_pass["a"+str(len(params)/2)],np.transpose(modified_y))
+		params = backward_prop_sigmoid(params,cost,forward_pass)
 	print(params)
 	
 if __name__ == "__main__":
